b9693e04a2d21026a189ee1558acea18a6d18b83
RoyceCho1 <roycecho@gm.gist.ac.kr>
2026-03-23 15:12:23 +0900

    feat: Enable SDPA attention implementation when loading the pretrained model.

--- llava_embeddings/pick_a_pick_user_emb_7b.py
+++ llava_embeddings/pick_a_pick_user_emb_7b.py
@@ -156,7 +156,7 @@ def main(_):
         tokenizer, model, image_processor, max_length = load_pretrained_model(
             pretrained, None, model_name,
             device_map=d_map,
-            attn_implementation=None,
+            attn_implementation="sdpa",
             quantization_config=quantization_config,
             **llava_model_args
         )
